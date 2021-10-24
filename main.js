a86e0f4f953db3295dd109aa9c85cb7cb5a63bef
nothinguy <80864088+nothinguy@users.noreply.github.com>
2021-10-24 14:16:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+song = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() {
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(400,600);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function draw(){
+image(video,0,0,600,400);
+
+}
+function modelLoaded(){
+
+    console.log('PoseNet is Intialized');
+}
+
+function play(){
+song.play(); 
+song.setVolume(1);
+song.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length > 0) {
+    
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+    
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    } 
+    
+    }
